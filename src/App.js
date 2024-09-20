60c2df3476896f549586fc3b170a4546484e0d8c
dharmendra <dharmendraraj776692@gmail.com>
2024-09-20 12:19:40 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './component/Home.jsx';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
+      </>
+    </BrowserRouter>
+  );
+}
+
+export default App;
